efe91bea40a398095a85b28bd394243bdda2ba29
guilherme.salmeida16 <guilherme.salmeida16@SBC0601503W10-1>
2022-05-05 12:01:05 -0300

    Adicionar arquivos de projeto.

---
+++ MenuTabuadaCPF/View/Tabuada/TelaTabuada.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace MenuTabuadaCPF.View.Tabuada
+{
+    public partial class TelaTabuada : Form
+    {
+        public TelaTabuada()
+        {
+            InitializeComponent();
+        }
+
+        private void CalcularTabuada()
+        {
+            int numero = 0;
+
+            numero = Convert.ToInt32(tbxNumero.Text);
+
+            for (int i = 0; i <= 10; i++)
+            {
+                lbxResultado.Items.Add(numero + " x " + i + " = " + i * numero);
+
+            }
+            
+        }
+
+        private void LimparTela()
+        {
+            lbxResultado.Items.Clear();
+           // tbxNumero.Clear();
+        }
+
+        private void tbxNumero_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar == 13)
+            {
+                LimparTela();
+                CalcularTabuada();
+                
+            }
+            
+
+        }
+    }
+}
